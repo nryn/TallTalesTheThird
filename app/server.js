ee02be8d84058bb86cd10386b9a0a1ec37f2b0d0
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-04-13 15:28:03 +0100

    Routes in server for accessing and setting story theme

--- app/server.js
+++ app/server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-//var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
 var Story = require('./story');
+var Theme = require('./theme');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 server.listen(3000);
 
@@ -10,6 +15,23 @@ console.log("Server up...")
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
+app.get('/theme', function(req, res) {
+  res.sendFile(__dirname + '/theme.html')
 });
+
+app.get('/index', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+  console.log(story)
+});
+
+app.post('/index', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+  story = new Story(Object.keys(req.body)[0])
+});
+
+
+io.sockets.on('connection', function(socket){
+  socket.on('client-theme-pic', function(data) {
+    console.log("hello")
+  })
+  });
