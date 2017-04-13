de147dac8544535b8abfbf1b49daad0bf8e36e49
nryn <narayanohanlon@gmail.com>
2017-04-13 11:02:43 +0100

    reverting last change - server can be setup with node app/server

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 //var io = require('socket.io').listen(server);
-var Story = require('./app/story');
+var Story = require('./story');
 
 server.listen(3000);
 
@@ -11,5 +11,5 @@ console.log("Server up...")
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/app/index.html')
+  res.sendFile(__dirname + '/index.html')
 });
